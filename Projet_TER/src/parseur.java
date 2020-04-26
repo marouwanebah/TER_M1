2e1fd5d730dd848c3619b0d25aeb1911cc050053
BAH <marouwanebah@gmail.com>
2020-04-26 15:54:14 +0200

    parsing mime

--- Projet_TER/src/parseur.java
+++ Projet_TER/src/parseur.java
@@ -1,30 +1,16 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.File;
+
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.ContentHandler;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Properties;
-
-import javax.lang.model.element.Element;
-import javax.lang.model.util.Elements;
 import javax.mail.Address;
-import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-
-import org.apache.james.mime4j.MimeException;
-import org.apache.james.mime4j.parser.MimeStreamParser;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 
 
@@ -33,87 +19,102 @@ public class parseur {
 
 	private static final String LIEN_FICHIER = "/home/etudiant/M1/S2/TER/Projet/TER_M1/Data/president/";  
 	
-	public static void main (String[] args) throws IOException, MimeException, MessagingException{
-		/*
-		BufferedReader fichierEntre = new BufferedReader(new FileReader(LIEN_FICHIER+17));
-		String line;
+	
+	
+	/*
+	 * Fonction qui recupere toutes les infos  du mail et qui crée un object 
+	 * Maillist qui a toutes des informations du mail 
+	 */
+	
+	public static MailList ParseMail(String messagePath) throws MessagingException, IOException {
 		
-		while ((line = fichierEntre.readLine()) != null)
-		{
-			// Afficher le contenu du fichier
-			System.out.println (line);
+	    InputStream mailFileInputStream = new FileInputStream(messagePath);    
+	    Properties props = System.getProperties(); 
+	    Session session = Session.getInstance(props, null);
+	    MimeMessage message = new MimeMessage(session, mailFileInputStream);
 
-		}
-		fichierEntre.close();
-		
-		
-		File input = new File(LIEN_FICHIER+17);
-
-		Document doc =Jsoup.parse(input,"UTF-8");
-		String body = doc.body().text();
-		String title = doc.title(); 
-		//String text = doc.body().text();
-		org.jsoup.nodes.Element content =doc.body();
- 
-		System.out.print(title);
-		System.out.print(body);
-		
-		 
-		//MimeMessage a = Mime;  
-		
-
-		MimeStreamParser parser = new MimeStreamParser();
-		InputStream instream = new FileInputStream(LIEN_FICHIER+17);
+	    String body = "";
+	    String from = "";
+	    ArrayList<String> destinataire = new ArrayList<String>(); 
+	    ArrayList<MimeBodyPart> attachments = new ArrayList<MimeBodyPart>();         
+	    String contentType = message.getContentType();
+	        Address[] addresses = message.getFrom();
+	        Address[] AdresDestinatire = message.getAllRecipients(); 
 	        
-		parser.parse(instream);	     
-		instream.close();
-	      
-	    	
-		 StringBuffer fileData = new StringBuffer(1000);
-		    BufferedReader reader = new BufferedReader(new FileReader(new File(LIEN_FICHIER+17)));
-		    char[] buf = new char[1024];
-		    int numRead = 0;
-		    while ((numRead = reader.read(buf)) != -1) {
-		        fileData.append(buf, 0, numRead);
-		    }
-		    reader.close();
+	        for(int num = 0; num < AdresDestinatire.length ; num++) {
+	        	destinataire.add(AdresDestinatire[num].toString()); 
+	        }
 
-  */
-
-		// Create a MimeMessage
-
-		    InputStream mailFileInputStream = new FileInputStream(LIEN_FICHIER+14);    
-		    Properties props = System.getProperties(); 
-		    Session session = Session.getInstance(props, null);
-		    MimeMessage message = new MimeMessage(session, mailFileInputStream);
-
-		    Address[] destinataire = message.getAllRecipients(); 
-		    Date DateEnvoie = message.getSentDate(); 
-		    Address[] sender = message.getFrom(); 
-		    String sujet = message.getSubject();
-		    Object contenu  = message.getDataHandler().getContent(); 
-		    
-		    
-		    for( int i=0; i<destinataire.length; ++i) {
-		    	System.out.println( "Destinataire : "+ destinataire[i]);
-		    }
-		    for( int i=0; i<sender.length; ++i) {
-		    	System.out.println("sender :"+ sender[i]);
-		    }
-		    
-		    System.out.println(DateEnvoie);
-		    System.out.println("sujet :" + sujet);
-		    
-			File input = new File(LIEN_FICHIER+14);
-
-			Document doc =Jsoup.parse(input,"UTF-8");
-			//exemple de recuperation de lien 
-			//Elements tex = doc.select("a"); 
-			for (org.jsoup.nodes.Element a: doc.select("a")) {
-				System.out.println(a.attr("href"));
-			}
-
-		  //  System.out.println(doc.ti);
+	        
+	        
+	        if(addresses.length == 1)
+	            from = addresses[0].toString();
+	        else
+	        {
+	            for(int num = 0; num < addresses.length - 1; num++)
+	                from += addresses[num].toString() + ", ";
+	            from += addresses[addresses.length].toString();
+	        }
+	        if(contentType.contains("text/plain"))
+	        {  
+	        	System.out.println("text plain detected ");
+	            Object content = message.getContent();
+	            if(content != null)
+	                body += content.toString();
+	        }
+	        if(contentType.contains("text/html"))
+	        {
+	        	System.out.println("text/html detected ");
+	        	
+	            Object content = message.getContent();
+	            if(content != null)
+	                body += Jsoup.parse((String)content).text();
+	        }
+	        if(contentType.contains("multipart"))
+	        {  
+	        	System.out.println("mutipart");
+	            Multipart mp = (Multipart)message.getContent();
+	            int numParts = mp.getCount();
+	           
+	            for(int count = 0; count < numParts; count++)
+	            {
+	    
+	                MimeBodyPart part = (MimeBodyPart)mp.getBodyPart(count);
+	                String content = part.getContent().toString();
+	                if(MimeBodyPart.ATTACHMENT.equalsIgnoreCase(part.getDisposition()))
+	                    attachments.add(part);
+	                else if(part.getContentType().contains("text/html"))
+	                    body += Jsoup.parse(content).text();
+	                else
+	                    body += content;
+	            }
+	        }
+	        MailList a = new MailList(from, destinataire, message.getSubject(), body,
+	                message.getSentDate().toString(), attachments);
+	    
+/*
+	        System.out.println(a.toString());
+	        for (MimeBodyPart part : a.getAttachments() ) {
+	        	System.out.println(part.getContent());
+	        }
+*/        
+	        return a; 
+		
+	}
+	
+	public static void main (String[] args) throws MessagingException, IOException {
+		
+		ArrayList<MailList> listeMail = new ArrayList<MailList>(); 
+		
+		for(int i = 1; i<32; ++i) {
+			listeMail.add(ParseMail(LIEN_FICHIER+i)); 
+		}
+		int i=1; 
+		for(MailList a : listeMail) {
+			System.out.println(a.getBody());
+			System.out.println("-------------"+(i++)+"--------------");
+		}
+		
 	}
 	
 }
