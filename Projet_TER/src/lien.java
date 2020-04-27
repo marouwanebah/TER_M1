9dffc9f4ef155fa3caa20ab6dcb50ac70837b284
BAH <marouwanebah@gmail.com>
2020-04-27 13:06:46 +0200

    parsing attachement, signature and link

---
+++ Projet_TER/src/lien.java
@@ -0,0 +1,31 @@
+
+public class lien {
+	private String _lien; 
+	private String _ContenueLien;
+	
+	public lien(String _lien, String _ContenueLien) {
+		super();
+		this._lien = _lien;
+		this._ContenueLien = _ContenueLien;
+	}
+
+	public String get_lien() {
+		return _lien;
+	}
+
+	public void set_lien(String _lien) {
+		this._lien = _lien;
+	}
+
+	public String get_ContenueLien() {
+		return _ContenueLien;
+	}
+
+	public void set_ContenueLien(String _ContenueLien) {
+		this._ContenueLien = _ContenueLien;
+	} 
+	
+	
+	
+
+}
